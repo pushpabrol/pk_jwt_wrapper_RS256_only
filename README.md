2c10027735d7588de604e2efa8ccc9cfc3d1f228
Pushp Abrol <pushp.abrol@gmail.com>
2024-11-27 15:05:23 -0500

    first commit

--- README.md
+++ README.md
@@ -49,46 +49,29 @@ GET /.well-known/keys
 
 ```
 
-### 3. `/intermediary.jwks` (GET)
-
-This endpoint provides the public keys for the Auth0 Connection to use as the JWKS endpoint. Since the IDP is sending a token signed with RS512 the wrapper verifies the tokens, makes all the checks etc and then creates a new token signed with RS256. To verify this token auth0 needs a jwks and this url provides that. We will also update the connection to use this as the JWKs instead of the one provided by the IDP
-
-Example Request:
-```json
-GET /intermediary.jwks
-
-```
 
 ## Configuration
 
 Before running the server, configure the required environment variables in the `.env` file or in vercel. Here are the environment variables you need to set:
 
+
 - `RP_ID` - <Client_id from the IDP>
 - `A0_CLIENT_SECRET` - <a client secret you use in the token wrapper to make sure its only called from auth0 ( shared secret with auth0)>
 - `RP_PRIVATE_KEY_RS256` - "pkcs8 formattted private key - RS256"
-- `RP_PRIVATE_KEY_RS512` - "pkcs8 formattted private key - RS512"
 - `RP_KID_RS256` - <kid for RS256>
-- `RP_KID_RS512` - <kid for RS512>
 - `IDP_DOMAIN` - domain of your IDP
 - `IDP_TOKEN_ENDPOINT` - path of your IDP's token endpoint relative to the domains based url - /token
-- `IDP_TOKEN_SIGNING_ALG` - Algorithm used by the IDP to sign the id_token
 - `RP_CLIENT_ASSERTION_SIGNING_ALG` - Algorithm used by the RP/this wrapper to sign the client authentication assertion
 - `DEBUG` - false or true
-- `INTERMEDIARY_PRIVATE_KEY` - "pkcs8 formattted private key - RS256, used by the wrapper to sign the token with RS256 for auth0 connection"
-- `INTERMEDIARY_KEY_KID` - "kid for RS256 intermediary"
-- `INTERMEDIARY_SIGNING_ALG` - RS256 <for auth0 this is RS256>
-- `IDP_JWKS_ENDPOINT` - path of your IDP jwks endpoint relative to the domains based url - /.well-known/jwks.json
 
 
-Ensure that the `RP_PRIVATE_KEY_RS256` and/or `RP_PRIVATE_KEY_RS512` `INTERMEDIARY_PRIVATE_KEY` `INTERMEDIARY_KEY_KID` and `RP_CLIENT_ASSERTION_SIGNING_ALG`  environment variables are set according to your private key and algorithm used for generating client assertions.
+Ensure that the `RP_PRIVATE_KEY_RS256` and `RP_CLIENT_ASSERTION_SIGNING_ALG`  environment variables are set according to your private key and algorithm used for generating client assertions.
 
-Ensure that the `IDP_TOKEN_SIGNING_ALG` is set to what matches the IDP uses for signing the id_token
 
-In the env file the `RP_PRIVATE_KEY_RS256` or `RP_PRIVATE_KEY_RS512` is the PKCS8 formatted Private key with newlines replaced with `\n` . Example ....-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC1dsvQ6S79NM+U\n...gEFVWzotcHeRbyso8nNEeF10JBPY2qvNOveLsV9WFQhwG6+vFtski1VpjYpucjaN\nadx4UD2Hw8MYvwdkG7BpFA==\n-----END PRIVATE KEY-----\n
+In the env file the `RP_PRIVATE_KEY_RS256` is the PKCS8 formatted Private key with newlines replaced with `\n` . Example ....-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC1dsvQ6S79NM+U\n...gEFVWzotcHeRbyso8nNEeF10JBPY2qvNOveLsV9WFQhwG6+vFtski1VpjYpucjaN\nadx4UD2Hw8MYvwdkG7BpFA==\n-----END PRIVATE KEY-----\n
 
 The `spkis/relyingPartyJWKS.json` file contains the public key(s) in jwks format that gets exposed as /.well-known/keys for the IDP to use for client assertion verification. If your IDP uses `jwks_uri` for client assertion validation this url can be used or else you can share the public key with them based on that jwks in this file. Make sure you set the contents of this file based on the public keys you have for client asertion validation by the IDP
 
-The `spkis/intermediaryJWKS.json` file contains the public key(s) in jwks format that gets exposed as `/intermediary.jwks` for Auth0 to use for token verification of the RS256 token created in the wrapper
 
 ## Use in Auth0 connection ( Example)
 
@@ -101,28 +84,29 @@ Assume your IDP's url is https://idp.com
   "options": {
     "type": "back_channel",
     "scope": "openid profile email",
-    "issuer": "https://idp.com",
-    "jwks_uri": "https://<your token wrapper's domain>/intermediary.jwks",
-    "client_id": "client_pk_kwt",
+    "issuer": "https://login.pushp.me",
+    "jwks_uri": "https://login.pushp.me/jwks",
+    "client_id": "client_pk_jwt_RS256",
     "attribute_map": {
       "mapping_mode": "bind_all"
     },
     "client_secret": "e7b613fc-68df-480c-855b-e6ae8b15e44d",
     "schema_version": "openid-1.0.0",
-    "token_endpoint": "https://<your token wrapper's domain>/token",
-    "userinfo_endpoint": "https://idp.com/me",
+    "token_endpoint": "https://pk-jwt-rs256-proxy.vercel.app/token",
+    "userinfo_endpoint": "https://login.pushp.me/me",
     "connection_settings": {
       "pkce": "auto"
     },
-    "authorization_endpoint": "https://idp.com/auth"
+    "authorization_endpoint": "https://login.pushp.me/auth"
   },
   "strategy": "oidc",
-  "name": "<your connection's name in auth0>",
+  "name": "login-idp-RS256-client-auth-only",
   "is_domain_connection": false,
   "show_as_button": false,
-  "display_name": "<your connection's display name in auth0>",
+  "display_name": "Login with idp using PK JWT Auth",
   "enabled_clients": [
-    "<client for which this connection is enabled>(optional)"
+    "srwri08zqO2Z6OGuFuM7f0iaWDJrRQ2B",
+    "I3hzldSdp2kj8Hozsn0q7un03UEDk82j"
   ]
 }
 ```
--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const axios = require('axios'); // HTTP client for making requests
 const uuid = require('uuid'); // Universally Unique Identifier (UUID) generator
 const dotenv = require('dotenv'); // Load environment variables from a .env file
 const qs = require('querystring'); // Query string parsing and formatting
-const oidcTokenHash = require('oidc-token-hash');
 const decode = (input) => Buffer.from(input, 'base64');
+const crypto = require('crypto');
+
 
 const relyingPartyJWKS = require('./spkis/relyingPartyJWKS.json');
-const intermediaryJWKS = require('./spkis/intermediaryJWKS.json');
 
 dotenv.config(); // Load environment variables from the .env file
 process.env.RP_CLIENT_ASSERTION_SIGNING_ALG = process.env.RP_CLIENT_ASSERTION_SIGNING_ALG || "RS256";
@@ -29,98 +29,54 @@ app.post('/token', async (req, res) => {
     const context = process.env;
     LOG(req.body);
 
-    // Retrieve parameters from the request body
     const { client_id, code, redirect_uri, code_verifier, client_secret } = req.body;
 
-    // Check if the client_id is missing
     if (!client_id) {
         return res.status(400).send('Missing client_id');
     }
 
     if (client_secret && client_secret !== context.A0_CLIENT_SECRET) return res.status(400).send('client auth failed by auth0!');
 
-    // Check if the provided client_id matches the expected one
     if (context.RP_ID === client_id) {
         try {
-            // Generate a client_assertion (JWT) for client authentication
             const client_assertion = await generatePrivateKeyJWTForClientAssertion(context);
             LOG(client_assertion);
 
-            var data = {
+            const data = {
                 grant_type: 'authorization_code',
                 client_id: context.RP_ID,
                 client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
                 client_assertion,
                 code,
-                redirect_uri
+                redirect_uri,
+                ...(code_verifier && { code_verifier })
             };
 
-            if (code_verifier) data.code_verifier = code_verifier;
-
-            // Prepare the request to exchange the authorization code for tokens
             const options = {
                 method: 'POST',
                 url: `https://${context.IDP_DOMAIN}${context.IDP_TOKEN_ENDPOINT}`,
                 headers: { 'content-type': 'application/x-www-form-urlencoded' },
-                data: qs.stringify({
-                    grant_type: 'authorization_code',
-                    client_id: context.RP_ID,
-                    client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-                    client_assertion,
-                    code_verifier,
-                    code,
-                    redirect_uri
-                }),
+                data: qs.stringify(data),
             };
 
-            // Send the token exchange request to the authorization server
             const response = await axios.request(options);
             LOG(response.data);
 
-            // Extract the id_token from the response
             const { id_token } = response.data;
 
             const header = JSON.parse(decode(id_token.split('.')[0]));
 
-            //It's not an RS256 signed id token
-            if (header.alg !== context.INTERMEDIARY_SIGNING_ALG) {
-                // It's not signed with the algo I expected it to be
-                if (header.alg !== context.IDP_TOKEN_SIGNING_ALG) return res.status(500).send(`id_token signing token algorithm mismatch, expected ${context.IDP_TOKEN_SIGNING_ALG} , got: ${header.alg}`);
-
-                const publicKeyIDP = createRemoteJWKSet(new URL(`https://${context.IDP_DOMAIN}${context.IDP_JWKS_ENDPOINT}`))
-
-                // Verify the id_token with the public key
-                const { payload, protectedHeader } = await jwtVerify(id_token, publicKeyIDP, {
-                    issuer: `https://${context.IDP_DOMAIN}`,
-                    audience: context.IDP_CLIENT_ID,
-                });
-
-                LOG(payload);
-                LOG(protectedHeader);
-                // Remove the nonce from the payload and replace the id_token with a new RS256 token
-                if (payload.nonce) delete payload.nonce;
-                if (payload.at_hash && response.data.access_token) {
-                    const at_hashCalc = oidcTokenHash.generate(response.data.access_token, protectedHeader.alg);
-                    LOG(at_hashCalc);
-                    if (at_hashCalc === payload.at_hash) {
-                        const at_hashCalcRS256 = oidcTokenHash.generate(response.data.access_token, "RS256");
-                        payload.at_hash = at_hashCalcRS256;
-                    } else return res.status(500).send(`at_hash mismatch, expected ${payload.at_hash} , got: ${at_hashCalc}`);
-                }
-                response.data.payload = payload;
-                delete response.data.id_token;
-
-                // Generate an RS256 token from the payload for auth0
-                const jwt = await generateRS256Token(payload, context);
-                response.data.id_token = jwt;
+            // If the token is signed with RS256, pass it as is
+            if (header.alg === 'RS256') {
+                return res.status(200).send(response.data);
             }
-            // Send the response with the updated id_token
-            return res.status(200).send(response.data);
+
+            // Handle tokens signed with other algorithms
+            return res.status(500).send(`Unsupported signing algorithm: ${header.alg}`);
 
 
         } catch (error) {
             if (error.response) {
-                // Handle errors with HTTP responses
                 return res.status(error.response.status).send(error.response.data);
             } else {
                 console.error('Error:', error.message);
@@ -128,21 +84,27 @@ app.post('/token', async (req, res) => {
             }
         }
     } else {
-        // Return an error response for invalid client_id
         return res.status(401).send('Invalid request, client_id is incorrect!');
     }
 });
 
-// Create a route for /.well-known/keys
-// Used by the relying party of IDP to provide an ES256 public key for client authentication
+
+const jwksETag = crypto.createHash('sha256').update(JSON.stringify(relyingPartyJWKS)).digest('base64');
+
 app.get('/.well-known/keys', async (req, res) => {
+    // Check if the client's cached version matches the current ETag
+    if (req.headers['if-none-match'] === jwksETag) {
+        // If the ETag matches, send a 304 Not Modified response
+        return res.status(304).send();
+    }
+
+    res.set({
+        'Cache-Control': 'public, max-age=3600', // Cache for 1 hour
+        'Content-Type': 'application/json',
+        'ETag': jwksETag, // Provide ETag for cache validation
+    });
     res.json(relyingPartyJWKS);
 });
-
-app.get('/intermediary.jwks', async (req, res) => {
-    res.json(intermediaryJWKS);
-});
-
 // Start the Express server and listen on the specified port
 app.listen(port, () => {
     LOG(`Server is listening at http://localhost:${port}`);
@@ -186,39 +148,8 @@ async function generatePrivateKeyJWTForClientAssertion(context) {
     }
 }
 
-// Function to generate an RS256 token by the intermediary
-async function generateRS256Token(payload, context) {
-    if (payload.nonce) delete payload.nonce;
-    try {
-        const key = await loadRS256PrivateKey(context);
-        console.log(key);
-        const jwt = await new SignJWT(payload)
-            .setProtectedHeader({ alg: context.INTERMEDIARY_SIGNING_ALG, kid: context.INTERMEDIARY_KEY_KID, typ: "JWT" })
-            .setIssuedAt()
-            .setIssuer(`https://${context.IDP_DOMAIN}`)
-            .setAudience(context.RP_ID)
-            .setExpirationTime('2m') // Expiration time
-            .setJti(uuid.v4())
-            .sign(key);
-        console.log(jwt);
-        return jwt;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
-}
 
-// Function to load the RS256 private key
-async function loadRS256PrivateKey(context) {
-    try {
-        var privateKey = context.INTERMEDIARY_PRIVATE_KEY.replace(/\n/g, "\r\n");
-        var key = await importPKCS8(privateKey, context.INTERMEDIARY_SIGNING_ALG);
-        return key;
-    } catch (e) {
-        console.log(e);
-        return e;
-    }
-}
+
 
 
 
